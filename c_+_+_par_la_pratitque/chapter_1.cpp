d654a1012e2225dd104b79da76ec5f875c012994
mbthebti <mohamed.thebti@aurorasgrid.com>
2026-09-20 21:39:36 +0200

    book tutorial

---
+++ c_+_+_par_la_pratitque/chapter_1.cpp
@@ -0,0 +1,23 @@
+//
+// Created by moham on 20.09.2026.
+//
+
+/*
+multi line comment
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int i;
+
+double x(3.14);
+
+double j(7.6);
+i=j+3;
+
+
+main() {
+ cout << "Let's start!" << endl;
+}
