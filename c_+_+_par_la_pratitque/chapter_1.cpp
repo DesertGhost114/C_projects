5ab449aa4fe528d4693831fc96f88d21649d8f90
mbthebti <mohamed.thebti@aurorasgrid.com>
2026-09-21 12:33:14 +0200

    ,

--- c_+_+_par_la_pratitque/chapter_1.cpp
+++ c_+_+_par_la_pratitque/chapter_1.cpp
@@ -15,9 +15,28 @@ int i;
 double x(3.14);
 
 double j(7.6);
-i=j+3;
+
+int k(6);
+
+int l = k+3;
 
 
-main() {
+int main() {
+ double temperature;
+ double temperature_user;
+
+ temperature = 22.6;
+
  cout << "Let's start!" << endl;
+
+ cout << "Please enter the estimated room temperature :" << endl;
+ cin >> temperature_user;
+ if (temperature_user == temperature) {
+  cout << "You guessed correctly. The room temperature is indeed "<< temperature << " degrees (°C)." << endl;
+ }
+ else {
+  cout << "Current room temperature is "<< temperature << " degrees (°C)." << endl;
+  cout << "You thought it was "<< temperature_user << " degrees (°C)." << endl;
+ }
+
 }
